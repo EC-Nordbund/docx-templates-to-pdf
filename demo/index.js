767dc3261b7585265b628ad3d5d760cdd0cc3a98
Sebastian Krüger <2pi_r2@gmx.de>
2020-12-05 18:54:27 +0100

    release 1.0.0

--- demo/index.js
+++ demo/index.js
@@ -1,19 +1,19 @@
 const { readFileSync, createWriteStream } = require("fs");
-const { fillDocToPdf, fillDocsToPdf } = require("..");
+const { createGotenberg } = require("..");
 
 const file = readFileSync('./demo/testdoc.docx');
-
+const gotenberg = createGotenberg('https://gotenberg.ec-nordbund.de');
 
 (async () => {
   // Single Document
-  const f = await fillDocToPdf('https://gotenberg.ec-nordbund.de', file, { version: 1 })
+  const f = await gotenberg(file, { version: 1 })
 
   f.pipe(createWriteStream('./out.pdf'))
 
   // Multiple Document
   const data = new Array(2).fill(0).map(v => ({ version: Math.random() }))
-  
-  const g = await fillDocsToPdf('https://gotenberg.ec-nordbund.de', file, data)
+
+  const g = await gotenberg(file, data)
   g.pipe(createWriteStream('./out2.pdf'))
 })()
 
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,31 @@
 import { createReport } from "docx-templates";
-import { gotenberg, pipe, office, please, convert, set, filename, to, portrait, merge } from "gotenberg-js-client";
+import { gotenberg, pipe, office, please, convert } from "gotenberg-js-client";
 import { IncomingMessage } from "http";
 
-async function fillDocx(file: Buffer, data: any): Promise<ArrayBuffer> {
+async function fillDoc(file: Buffer, data: any): Promise<ArrayBuffer> {
   return (await createReport({
     template: file,
     data,
-  })).buffer as ArrayBuffer
-}
-
-function convertDocx2Pdf(url: string, file: ArrayBuffer) {
-  return pipe(gotenberg(url), convert, set(filename('out.pdf')), office, please)(['test.docx', Buffer.from(file)]) as Promise<IncomingMessage>
-}
-
-function convertDocs2Pdf(url: string, file: ArrayBuffer[]) {
-  return pipe(gotenberg(url), convert, set(filename('out.pdf')), office, please)(file.map((v, i) => ([`${i}.docx`, Buffer.from(v)]))) as Promise<IncomingMessage>
-}
-
-
-export async function fillDocToPdf(url: string, file: Buffer, data: any) {
-  return (await convertDocx2Pdf(url, await fillDocx(file, data)))
+  })).buffer
 }
 
 function fillDocs(file: Buffer, data: any[]): Promise<ArrayBuffer[]> {
-  return Promise.all(data.map(v => fillDocx(file, v)))
+  return Promise.all(data.map(v => fillDoc(file, v)))
 }
 
-export async function fillDocsToPdf(url: string, file: Buffer, data: any[]) {
-  const filled = await fillDocs(file, data)
-  return await convertDocs2Pdf(url, filled)
+function convertDocx2Pdf(url: string, file: ArrayBuffer[] | ArrayBuffer) {
+  if (!Array.isArray(file)) {
+    file = [file]
+  }
+  return pipe(gotenberg(url), convert, office, please)(file.map((v, i) => ([`${i}.docx`, Buffer.from(v)])))
+}
 
-  // return pipe(gotenberg(url), merge, please)(finished.map((v, i) => ([`${i}.pdf`, v]))) as Promise<IncomingMessage>
+export function createGotenberg(url: string) {
+  return async function fillDocToPdf(file: Buffer, data: any | any[]) {
+    if (Array.isArray(data)) {
+      return convertDocx2Pdf(url, await fillDocs(file, data))
+    } else {
+      return convertDocx2Pdf(url, await fillDoc(file, data))
+    }
+  }
 }

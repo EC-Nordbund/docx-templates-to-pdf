9707d717b709a61c53c34f4c593e63cf4504e977
Sebastian Krüger <2pi_r2@gmx.de>
2020-12-13 08:39:32 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { createReport } from "docx-templates";
 import { UserOptions } from "docx-templates/lib/types";
-import { gotenberg, pipe, office, please, convert } from "gotenberg-js-client";
+import { gotenberg, pipe, office, please, convert, set, timeout } from "gotenberg-js-client";
 
 async function fillDoc(file: Buffer, data: any, config: Partial<UserOptions>): Promise<ArrayBuffer> {
   return (await createReport({
@@ -18,7 +18,7 @@ function convertDocx2Pdf(url: string, file: ArrayBuffer[] | ArrayBuffer) {
   if (!Array.isArray(file)) {
     file = [file]
   }
-  return pipe(gotenberg(url), convert, office, please)(file.map((v, i) => ([`${i}.docx`, Buffer.from(v)])))
+  return pipe(gotenberg(url), convert, office, set(timeout(15)), please)(file.map((v, i) => ([`${i}.docx`, Buffer.from(v)])))
 }
 
 export function createGotenberg(url: string) {
